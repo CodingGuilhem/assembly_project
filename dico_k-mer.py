d1e02c8cc04c92748407b8117600582563d6eb64
AmelChadli <pingu@LAPTOP-34OT5I82.>
2024-11-19 14:50:31 +0100

    ajout dico_k-mer

---
+++ dico_k-mer.py
@@ -0,0 +1,3 @@
+# on parcourt le fichier fasta
+# on ne lit que les lignes avec les sequences des reads
+# au fur et a mesure de la lecture des sequences, on recuperes les k-mers que l on place dans des dictionnaires
